5ccf30bf59b44e08ddc8c2c17d1e12be0eb2915a
avruch <yuvalavr@gmail.com>
2020-12-09 11:20:01 +0200

    second

--- scrape_news.sh
+++ scrape_news.sh
@@ -1,23 +1,31 @@
  #!/bin/bash 
+# first we access the website to gather the relevent articles
+wget https://www.ynetnews.com/category/3082 &> /dev/null
 
-wget https://www.ynetnews.com/category/3082
+grep -o -E '(https://www.ynetnews.com/article/)([0-9]|[a-z]|[A-Z]){9}(")+' 3082\
+	 | sort -u | sed 's/"//' > sorted.txt
+#we have generated file sorted.txt which has a list of unique urls 
+wc -l sorted.txt | sed s/sorted.txt// > results.csv
 
-grep -o -E '(https://www.ynetnews.com/article/)([0-9]|[a-z]|[A-Z]){9}(")+'3082|sort -u|sed 's/"//' >sorted.txt
+#now we run a while loop and open every single article to count the appearances
+# of bibi and beni in text
+while read line 
+do
+	wget -O temp.txt $line &> /dev/null
 
-wc -l sorted.txt| sed s/sorted.txt// >resuls.csv
+	bibi_count=$(grep -o 'Netanyahu' temp.txt | wc -l)
+	beni_count=$(grep -o 'Gantz' temp.txt | wc -l)
 
+	if   [[ "$bibi_count" == 0  &&  "$beni_count" == 0 ]]  ; then
 
-while read line <sorted.txt
-	wget -o temp.txt $line
+	 	 echo "${line}, -" >> results.csv 
 
-	Netanyhu_counter=$(grep -o 'Netanyhu' temp.txt|wc -l)
-	Gantz_counter=$(grep -o 'Gantz' temp.txt|wc -l)
+	else
 
-	if   [[$"Netanyhu_counter" ==0]&&[$"Gantz_counter" ==0]]&& echo "${line}, -">>resuls.csv
-
-	else 
-		 echo "${line},Netanyhu,${Netanyhu_counter},Gantz,${Gantz_counter}">>resuls.csv
+		 echo "${line}, Netanyahu, ${bibi_count}, Gantz, ${beni_count}"\
+		  >> results.csv
 	fi
  
-done
-
+done < sorted.txt
+#after program is done we remove the unncessary files created along the way
+rm 3082	temp.txt sorted.txt	
